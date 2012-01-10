ff5fe54ad5a12b8f811ef0a0bc0077bf06cce251
Coolkid <196774065@qq.com>
2012-01-11 00:30:21 +0800

    优化整理内存算法

--- mini/Memory.lua
+++ mini/Memory.lua
@@ -2,10 +2,18 @@ local eventcount = 0
 local a = CreateFrame("Frame") 
 a:RegisterAllEvents() 
 
-mem = GetAddOnMemoryUsage("s_Core")
 a:SetScript("OnEvent", function(self, event) 
    eventcount = eventcount + 1 
-   if  mem > 3072  then collectgarbage("collect")  eventcount = 0  end
+   if  GetAddOnMemoryUsage("s_Core") > 3072 then 
+		collectgarbage("collect")  
+		eventcount = 0 
+	end
+    if  IsAddOnLoaded("NugRunning") then
+		if  GetAddOnMemoryUsage("NugRunning") > 400 then
+			collectgarbage("collect") 
+			eventcount = 0 
+		end
+	end
    if InCombatLockdown() then return end 
    if eventcount > 6000 or event == "PLAYER_ENTERING_WORLD" then 
       collectgarbage("collect") 
