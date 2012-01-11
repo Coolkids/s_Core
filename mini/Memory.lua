0684c61be64baa39cd08b99d5f70446597f4f42f
Coolkid <196774065@qq.com>
2012-01-11 21:05:12 +0800

    ,.....

--- mini/Memory.lua
+++ mini/Memory.lua
@@ -4,10 +4,6 @@ a:RegisterAllEvents()
 
 a:SetScript("OnEvent", function(self, event) 
    eventcount = eventcount + 1 
-   if  GetAddOnMemoryUsage("s_Core") > 4072 then 
-		collectgarbage("collect")  
-		eventcount = 0 
-	end
    if InCombatLockdown() then return end 
    if eventcount > 6000 or event == "PLAYER_ENTERING_WORLD" then 
       collectgarbage("collect") 
