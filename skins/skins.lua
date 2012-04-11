4dc64dda5c9f714a00fb1a2e9ef733112a2fd832
Coolkid <s196774065@gmail.com>
2012-04-11 19:02:49 +0800

    蘑菇条 MSBT兼容性 动作条

--- skins/skins.lua
+++ skins/skins.lua
@@ -37,6 +37,24 @@ Delay:SetScript("OnEvent", function()
 		S.StripTextures(ChatBarFrame, Kill)
 		S.StripTextures(ChatBarFrameBackground, Kill)
 		ChatBarFrameBackground:CreateShadow("Background")
+		Button = CreateFrame("Button", nil, ChatBarFrameBackground, "SecureActionButtonTemplate")
+		Button:SetAttribute("*type*", "macro")
+		Button:SetAttribute("macrotext", "/roll")
+		Button:SetSize(ChatBarFrameButton1:GetWidth()*0.4, ChatBarFrameButton1:GetHeight()*0.4)
+		Button:SetBackdrop({ 
+			bgFile = DB.Statusbar
+		})
+		Button:CreateShadow("Background")
+		Button:SetBackdropColor(1, 1, 0)
+		Button:SetPoint("RIGHT", ChatBarFrameBackground, "RIGHT", -5, 0)
+		Button.Text = S.MakeFontString(Button, 7)
+		Button.Text:SetText("R")
+		Button.Text:SetTextColor(1, 215/255, 0)
+		Button.Text:SetPoint("BOTTOM", Button, "TOP", 2, 2)
+		local hover = Button:CreateTexture(nil, "OVERLAY")
+		hover:SetTexture(1, 1, 1, 0.3)
+		hover:SetAllPoints()
+		Button:SetHighlightTexture(hover)
 	end
 	
 	if ItemRefShoppingTooltip1 then 
@@ -47,7 +65,10 @@ Delay:SetScript("OnEvent", function()
 		S.StripTextures(ItemRefShoppingTooltip2)
 		ItemRefShoppingTooltip2:CreateShadow("Background")
 	end
-
+	
+	if UnitPopupMenus then
+		UnitPopupMenus["PLAYER"] = { "SET_FOCUS", "WHISPER", "INSPECT", "ACHIEVEMENTS", "INVITE", "TRADE", "FOLLOW", "DUEL", "RAID_TARGET_ICON", "RAF_SUMMON", "RAF_GRANT_LEVEL", "MOVE_PLAYER_FRAME", "MOVE_TARGET_FRAME", "CANCEL" };
+	end
 	
 	if DB.Nuke == true then
 		for i = 1,20 do
