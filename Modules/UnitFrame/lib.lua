9a3494968f0484fe650172260b3cce085d493dc6
Coolkid <s196774065@gmail.com>
2012-04-17 01:23:43 +0800

    fixbugs

--- Modules/UnitFrame/lib.lua
+++ Modules/UnitFrame/lib.lua
@@ -743,7 +743,7 @@ C = UnitFrameDB
                 i=i-1
             end
 	local function OnEvent(self,event,unit)
-		if ( event == "UNIT_AURA" and unit == "player" ) then
+		if ( event == "UNIT_AURA" and unit == "player" ) or event == "PLAYER_ENTERING_WORLD" then
 			local rank = select(4,UnitBuff("player", GetSpellInfo(77487)))
 			if rank then
 				for i = 1, rank do
@@ -920,7 +920,7 @@ end
         end
 		if 	class == "SHAMAN" then
 		local function OnEvent(self,event,unit)
-			if ( event == "UNIT_AURA" and unit == "player" ) then
+			if ( event == "UNIT_AURA" and unit == "player" ) or event == "PLAYER_ENTERING_WORLD" then
 				local rank = select(4,UnitBuff("player", GetSpellInfo(52127)))
 				if rank then
 					for i = 1, rank do
@@ -1004,17 +1004,15 @@ end
                 i=i-1
             end
 	local function OnEvent(self,event)
-		if ( event == "UNIT_AURA" and unit == "target" ) then
-			local rank = select(4,UnitBuff("target", GetSpellInfo(33763)))
-			local caster = select(8,UnitBuff("target", GetSpellInfo(33763)))
-			if rank and caster == "player" then
-				for i = 1, rank do
-					bars[i]:SetAlpha(1)
-				end
-			else
-				for i = 1, 3 do
-					bars[i]:SetAlpha(0)
-				end
+		local rank = select(4,UnitBuff("target", GetSpellInfo(33763)))
+		local caster = select(8,UnitBuff("target", GetSpellInfo(33763)))
+		if rank and caster == "player" then
+			for i = 1, rank do
+				bars[i]:SetAlpha(1)
+			end
+		else
+			for i = 1, 3 do
+				bars[i]:SetAlpha(0)
 			end
 		end
 	end
