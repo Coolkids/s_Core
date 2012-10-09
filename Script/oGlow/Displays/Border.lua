30e7149ad1edafbcd7aa6ee10bdd7bb3de1d419d
Coolkids <s196774065@gmail.com>
2012-10-09 19:50:22 +0800

    oGlow Aurora更新 修复不能显示外袍问题

--- Script/oGlow/Displays/Border.lua
+++ Script/oGlow/Displays/Border.lua
@@ -1,32 +1,18 @@
+local _, ns = ...
+local oGlow = ns.oGlow
 local S, _, _, _ = unpack(select(2, ...))
 local argcheck = oGlow.argcheck
-
-local colorTable = setmetatable(
-	{},
-	{__index = function(self, val)
-		argcheck(val, 2, "number")
-		local r, g, b = GetItemQualityColor(val)
-		rawset(self, val, {r, g, b})
-
-		return self[val]
-	end}
-)
+local colorTable = ns.colorTable
 
 local createBorder = function(self, point)
 	local bc = self.oGlowBorder
-	if not bc then
+	if(not bc) then
 		if(not self:IsObjectType'Frame') then
 			bc = CreateFrame("Frame", nil, self:GetParent())
 		else
 			bc = CreateFrame("Frame", nil, self)
 		end
-		bc:SetBackdrop({
-			edgeFile = "Interface\\ChatFrame\\ChatFrameBackground",   --, 
-			edgeSize = S.mult+0.2, 
-		})
-
-		
-
+		bc:CreateBorder()
 		bc:SetAllPoints(self)
 		self.oGlowBorder = bc
 	end
@@ -40,7 +26,6 @@ local borderDisplay = function(frame, color)
 		local rgb = colorTable[color]
 
 		if(rgb) then
-			--bc:SetVertexColor(rgb[1], rgb[2], rgb[3])
 			bc:SetBackdropBorderColor(rgb[1], rgb[2], rgb[3])
 			bc:Show()
 		end
@@ -51,19 +36,4 @@ local borderDisplay = function(frame, color)
 	end
 end
 
-function oGlow:RegisterColor(name, r, g, b)
-	argcheck(name, 2, "string", "number")
-	argcheck(r, 3, "number")
-	argcheck(g, 4, "number")
-	argcheck(b, 5, "number")
-
-	if(rawget(colorTable, name)) then
-		return nil, string.format("Color [%s] is already registered.", name)
-	else
-		rawset(colorTable, name, {r, g, b})
-	end
-
-	return true
-end
-
-oGlow:RegisterDisplay("Border", borderDisplay)
+oGlow:RegisterDisplay('Border', borderDisplay)
