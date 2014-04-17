4eea6c42842742ffb529a5b4ad67b63da1a53a3c
Coolkids <s196774065@gmail.com>
2014-04-17 22:47:55 +0800

    不能同时修理+卖东西的问题

--- misc/minitools/config.lua
+++ misc/minitools/config.lua
@@ -227,7 +227,6 @@ function MT:MERCHANT_SHOW()
 					if guildMoney > cost and CanGuildBankRepair() then
 						RepairAllItems(1)
 						DEFAULT_CHAT_FRAME:AddMessage("|cffffff00您修理装备花费了公会：|r"..format(GOLD_AMOUNT_TEXTURE, g, 0, 0).." "..format(SILVER_AMOUNT_TEXTURE, s, 0, 0).." "..format(COPPER_AMOUNT_TEXTURE, c, 0, 0),255,255,255)
-						return
 					else
 						RepairAllItems()
 					end
